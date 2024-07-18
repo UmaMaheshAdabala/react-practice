eea1454187d0a54e352788a1e988ae67d03777f4
Uma Mahesh <adabala.um@gmail.com>
2024-07-18 15:08:57 +0530

    chore:practice-1

---
+++ App.js
@@ -0,0 +1,6 @@
+// Nested tags
+
+const parent=React.createElement("div",{id:"parent"},React.createElement("div",{id:"child"},
+    [React.createElement("h1",{},"Hi im h1 tag"),React.createElement("h2",{},"h2 tag")]));
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
