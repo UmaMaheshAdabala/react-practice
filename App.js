7f1001e1bfc6127183b47a41fe7848c308d6f506
Uma Mahesh <adabala.um@gmail.com>
2024-07-18 16:22:33 +0530

    modified:README.md new file:   package-lock.json new file:   package.json modified:   App.js modified:   index.html

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
+import React from "react";
+import ReactDOM from"react-dom/client";
 // Nested tags
 
 const parent=React.createElement("div",{id:"parent"},React.createElement("div",{id:"child"},
-    [React.createElement("h1",{},"Hi im h1 tag"),React.createElement("h2",{},"h2 tag")]));
+    [React.createElement("h1",{},"Hi im h1 tag"),React.createElement("h2",{}," I'm a h2 tag")]));
 const root=ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
---
+++ README.md
@@ -0,0 +1,16 @@
+# Practice of React
+
+# Parcel 
+- A very powerful Bundler that used to bundle the code ready for production
+- It is an dev-dependency module
+- Local Server
+- HMR Hot Module replacement
+- File watching algorithm usin c++
+- Caching - Faster Builds
+- Image optimization
+- Minification
+- Bundling
+- Compress
+- Consistent hasing
+- code splitting
+
